351af963045318b9246cca57eaaf337b5da75f6a
NathalieLima <nathalielima3@gmail.com>
2023-12-01 00:43:53 -0300

    Subindo P003

---
+++ Introducao/instrucoes-praticas/P003/main.py
@@ -0,0 +1,18 @@
+def insertProduct(product):
+  pass
+
+def deleteProduct():
+  pass
+
+def listProducts():
+  pass
+
+def consultProducts():
+  pass
+
+def isValidCode(code):
+  return len(code) == 13 and code.isdigit()
+
+def isExistingProduct():
+  pass
+
